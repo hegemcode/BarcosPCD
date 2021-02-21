342b5b264d61b35c0bc801d312e431933b1123a6
pedrohegem <hegemcodes@gmail.com>
2021-02-21 11:32:58 +0100

    Revert "Paso 2"

--- src/main.java
+++ src/main.java
@@ -1,10 +1,9 @@
 
 public class main {
     public static void main(String[] args) {
-        Puerta puerta = new Puerta();
-        Thread t1 = new Thread(new Barco(1, true, puerta));
-        Thread t2 = new Thread(new Barco(2, false, puerta));
-        Thread t3 = new Thread(new Barco(3, false, puerta));
+        Thread t1 = new Thread(new Barco(1, true));
+        Thread t2 = new Thread(new Barco(2, false));
+        Thread t3 = new Thread(new Barco(3, false));
         t1.start();
         t2.start();
         t3.start();
