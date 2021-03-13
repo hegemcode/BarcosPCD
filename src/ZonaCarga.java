e414d5cfb162fa0dc3df3c67ff96ee7f2d633156
pedrohegem <hegemcodes@gmail.com>
2021-03-13 11:51:05 +0100

    Update ZonaCarga.java

--- src/ZonaCarga.java
+++ src/ZonaCarga.java
@@ -1,17 +1,25 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.concurrent.*;
 public class ZonaCarga {
     private static ZonaCarga z;
     private int contenedorAgua;
     private int[] contenedores_gas = new int[5];
     private Semaphore[] llegada = new Semaphore[5];
+    private Semaphore[] repostar = new Semaphore [5];
+    private Semaphore[] vacio = new Semaphore [5];
     private Semaphore mutex;
+    ArrayList <BarcoPetrolero> listaBarcos;
     int contadorLlegada = 0;
 
     private ZonaCarga(){
         mutex = new Semaphore(1);
         this.contenedorAgua = 1000000;
+        listaBarcos = new ArrayList<>();
         for(int i = 0; i < 5; i++){
             llegada[i] = new Semaphore(0);
+            vacio[i] = new Semaphore(0);
+            repostar[i] = new Semaphore(0);
         }
         for(int i = 0; i < 5; i++){
             contenedores_gas[i] = 1000;
@@ -27,6 +35,7 @@ public class ZonaCarga {
 
     public void llegar(BarcoPetrolero b) throws InterruptedException {
         mutex.acquire();
+        listaBarcos.add(contadorLlegada, b);
         contadorLlegada++;
         if(contadorLlegada != 5){
             System.out.println("Espera el barco " + b.getId() + " al resto de petroleros para repostar...");
@@ -39,13 +48,38 @@ public class ZonaCarga {
             mutex.release();
             for(int i = 0; i < contadorLlegada - 1; i++){
                 llegada[i].release();
+
             }
         }
-        // ZONA CRÍTICA
-        // repostarGas
+
+        while (b.getDeposito_gas() < 3000) {
+            repostarGas(b);
+        }
         repostarAgua(b);
+        //pedir salida
     }
 
+    public void repostarGas(BarcoPetrolero p) throws InterruptedException {
+        if( contenedores_gas[listaBarcos.indexOf(p)] > 0) {
+            p.setDeposito_gas(p.getDeposito_gas() + 1000);
+            contenedores_gas[listaBarcos.indexOf(p)] -= 1000;
+        }
+        if (contenedores_gas[listaBarcos.indexOf(p)] == 0) {
+            repostar[listaBarcos.indexOf(p)].release();
+            vacio[listaBarcos.indexOf(p)].acquire();
+        }
+    }
+
+    public void rellenarGas(int id) throws InterruptedException {
+        for(int i = 0; i < 5; i++) {
+            repostar[i].acquire();
+        }
+        for(int i = 0; i < 5; i++) {
+            contenedores_gas[listaBarcos.indexOf(id)] = 1000;
+            vacio[i].release();
+        }
+
+    }
     public void repostarAgua(BarcoPetrolero b){
         try {
             mutex.acquire();
